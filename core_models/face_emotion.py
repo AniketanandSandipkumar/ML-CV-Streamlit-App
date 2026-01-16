e44e3a9ba1d5c443d3452c7ba3ae8955b7bb386a
AniketanandSandipkumar <119923008+AniketanandSandipkumar@users.noreply.github.com>
2026-01-16 18:19:16 +0530

    Update face_emotion.py

--- core_models/face_emotion.py
+++ core_models/face_emotion.py
@@ -7,7 +7,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 MODEL_PATH = os.path.join(BASE_DIR, "face_emotion.h5")
 
-emotion_model = load_model(MODEL_PATH)
+emotion_model = load_model(MODEL_PATH, compile=False)
 
 # Emotion class mapping
 EMOTION_MAP = {
