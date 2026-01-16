11051fe2b094c1abb21f9f097924c48be7dc9710
AniketanandSandipkumar <119923008+AniketanandSandipkumar@users.noreply.github.com>
2026-01-16 18:18:48 +0530

    Update face_age.py

--- core_models/face_age.py
+++ core_models/face_age.py
@@ -7,8 +7,7 @@ import os
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 MODEL_PATH = os.path.join(BASE_DIR, "face_age.h5")
 
-age_model = load_model(MODEL_PATH)
-
+age_model = load_model(MODEL_PATH, compile=False)
 
 # Age class mapping 
 AGE_CLASS_MAP = {
